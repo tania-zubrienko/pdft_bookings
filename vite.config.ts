a0442f37040bba798ed5e3a7eca6ed2f45009e83
Tania Zubrienko <112280149+tania-zubrienko@users.noreply.github.com>
2026-02-21 09:56:14 +0100

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/pdft_bookings/',
   plugins: [react()],
   resolve: {
     alias: {
