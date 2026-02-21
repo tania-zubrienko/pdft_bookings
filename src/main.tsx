ac31dac3c5273384592a1fd464b1a6f2eb703df8
Tania Zubrienko <112280149+tania-zubrienko@users.noreply.github.com>
2026-02-21 10:01:33 +0100

    Set basename for BrowserRouter to '/pdft_bookings/'

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pdft_bookings/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
